28f544ff52f2d141dbbb8a1622175ad2850223c6
Brian Inguito <vincentvangoess@gmail.com>
2026-03-06 15:20:46 +0800

    fix; inventoryservice

--- src/views/animal/index.ts
+++ src/views/animal/index.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+export { default as AnimalAdmin } from "./AnimalAdmin";
 export { default as AnimalMonitoring } from "./AnimalMonitoring";
 export { default as AnimalTagging } from "./AnimalTagging";
 export { default as Breeding } from "./Breeding";
 export { default as Feeding } from "./Feeding";
 export { default as Inventory } from "../inventory/Inventory";
 export { default as Vitamins } from "./Vitamins";
-=======
-export { default as AnimalAdmin } from './AnimalAdmin'
-export { default as AnimalMonitoring } from './AnimalMonitoring'
-export { default as AnimalTagging } from './AnimalTagging'
-export { default as Breeding } from './Breeding'
-export { default as Storage } from './Storage'
-export { default as Inventory } from '../inventory/Inventory'
->>>>>>> 74af069f676d8dd8f8795b472e744f1577620b2a
