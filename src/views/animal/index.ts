b8460027aaad2b3dbcc4d7bfe80a85b1a5011046
James España <148767761+jmsespana@users.noreply.github.com>
2026-03-06 14:54:06 +0800

    Refactor code structure for improved readability and maintainability

--- src/views/animal/index.ts
+++ src/views/animal/index.ts
@@ -1,6 +1,5 @@
 export { default as AnimalMonitoring } from './AnimalMonitoring'
 export { default as AnimalTagging } from './AnimalTagging'
 export { default as Breeding } from './Breeding'
-export { default as Feeding } from './Feeding'
-export { default as Inventory } from './Inventory'
-export { default as Vitamins } from './Vitamins'
+export { default as Storage } from './Storage'
+export { default as Inventory } from '../inventory/Inventory'
